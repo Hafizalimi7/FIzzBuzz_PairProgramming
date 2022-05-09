f0328c99ec16925bf8168b6352c0256333cbb558
Hafiz <hafizalimi77@gmail.com>
2022-05-09 15:18:56 +0100

    Test passed for number 1-100

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,18 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it "Runs fizzbuzz for numbers 1 to 100" do
+      i = 1
+      while i <= 100 do
+        if (i % 3 == 0) && (i % 5 == 0) 
+          expect(fizzbuzz(i)).to eq "fizzbuzz"
+        elsif i % 3 == 0
+          expect(fizzbuzz(i)).to eq "fizz"
+        elsif i % 5 == 0
+          expect(fizzbuzz(i)).to eq "buzz"
+        else
+          expect(fizzbuzz(i)).to eq i
+        end
+        i += 1
+      end
   end
 end
-
-
-
-
-
-
